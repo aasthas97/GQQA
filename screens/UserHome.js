d2c6f2cc7e263f15c42ef70c5d037aa9ec7405da
Aastha <34710946+aasthas97@users.noreply.github.com>
2022-01-06 10:29:23 +0530

    Made functions in-line
    Instead of creating separate logout and navigation functions, they've been passed as arrow functions

--- screens/UserHome.js
+++ screens/UserHome.js
@@ -2,34 +2,33 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import Header from '../components/Header';
 import CustomButton from '../components/CustomButton';
-import Footer from '../components/Footer';
 import GlobalStyle from '../utils/Style';
 
 const UserHome = ({navigation}) => {
-  function goToAssessment() {
-    navigation.navigate('Assessment');
-  }
-
-  function logOut() {
-    // console.log('Signing out..');
-    navigation.navigate('SignIn');
-  }
-
   return (
     <View style={GlobalStyle.container}>
-      <Header signedIn={true} logOutFunc={logOut} />
+      <Header
+        signedIn={true}
+        logOutFunc={() => {
+          navigation.navigate('SignIn');
+        }}
+      />
       <View style={[GlobalStyle.body, {width: '90%', alignItems: 'center'}]}>
         <Text style={GlobalStyle.headingText}>HOME</Text>
 
         <CustomButton
-          onPress={() => {}}
-          displayText={'DEMO'}
+          onPress={() => {
+            navigation.navigate('Dimension');
+          }}
+          displayText={'DIMENSION ANALYSIS'}
           buttonWidth="90%"
           buttonHeight="8%"
         />
         <CustomButton
-          onPress={goToAssessment}
-          displayText={'ASSESSMENT'}
+          onPress={() => {
+            navigation.navigate('Assessment');
+          }}
+          displayText={'QUALITY ANALYSIS'}
           buttonWidth="90%"
           buttonHeight="8%"
         />
