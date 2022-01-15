91deda9060bd070da0d06b2631a5725b6250d2bf
Aastha S <aasthashar@gmail.com>
2022-01-15 12:24:21 +0530

    Display averages

--- screens/DimensionResult.js
+++ screens/DimensionResult.js
@@ -4,16 +4,22 @@ import {Table, Row} from 'react-native-table-component';
 import globalStyle from '../utils/Style';
 
 export default DimensionResult = ({route}) => {
-  const lengths = route.params['Length'];
-  const breadths = route.params['Breadth'];
-  const areas = route.params['Area'];
-  const perimeters = route.params['Perimeter'];
+  const lengths = route.params['df']['Length'];
+  const breadths = route.params['df']['Breadth'];
+  const perimeters = route.params['df']['Perimeter'];
+  const areas = route.params['df']['Area'];
   let tableData = [];
 
   for (let i = 0; i < lengths.length; i++) {
     let rowData = [i + 1, lengths[i], breadths[i], perimeters[i], areas[i]];
     tableData.push(rowData);
   }
+
+  const avg_length = route.params['average_length'];
+  const avg_breadth = route.params['average_breadth'];
+  const avg_perimeter = route.params['average_perimeter'];
+  const avg_area = route.params['average_area'];
+  tableData.push(['Average', avg_length, avg_breadth, avg_perimeter, avg_area]);
 
   return (
     <ScrollView style={globalStyle.container}>
